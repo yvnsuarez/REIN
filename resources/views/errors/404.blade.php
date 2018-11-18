cd4a5e2ec90d7f9140be184d6639cc27f88658e3
yvnsuarez <yvone.suarez@benilde.edu.ph>
2018-11-18 22:16:13 +0800

    404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -25,26 +25,9 @@
     <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
 <![endif]-->
 </head>
-
-<body class="fix-header fix-sidebar">
-    <!-- Preloader - style you can find in spinners.css -->
-    <div class="preloader">
-        <svg class="circular" viewBox="25 25 50 50">
-			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
-    </div>
-    <!-- Main wrapper  -->
-    <div class="error-page" id="wrapper">
-        <div class="error-box">
-            <div class="error-body text-center">
-                <h1>404</h1>
+<body>
                 {{ $exception->getMessage() }}
-                <h3 class="text-uppercase">The page you are looking for Doesn't exist</h3>
-                {{-- <p class="text-muted m-t-30 m-b-30">Bo back to the home page.</p> --}}
-                <a class="btn btn-info btn-rounded waves-effect waves-light m-b-40" href="{{ url()->previous() }}">Back to home</a> </div>
-            <footer class="footer text-center">&copy; 2018 Zebramin.</footer>
-        </div>
-    </div>
-
+                <h3>Hello World!</h3>
 
 </body>
 
