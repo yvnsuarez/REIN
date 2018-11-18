879db5248726819dfe971a929cfababac9bda5cd
yvnsuarez <yvone.suarez@benilde.edu.ph>
2018-11-18 22:09:19 +0800

    error 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>REIN</title>
+    <meta name="description" content="Ela Admin - HTML5 Admin Template">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="{{ asset('assets/images/justREIN.png') }}"/>
+    <link rel="shortcut icon" href="{{ asset('assets/images/justREIN.png') }}"/>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/normalize.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/bootstrap.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/font-awesome.min.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/themify-icons.css') }}"/>
+    <link rel="stylesheet" href="{{ asset('assets/css/pe-icon-7-filled.css') }}"/>
+
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"/>
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:** -->
+    <!--[if lt IE 9]>
+    <script src="https:**oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https:**oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+</head>
+
+<body class="fix-header fix-sidebar">
+    <!-- Preloader - style you can find in spinners.css -->
+    <div class="preloader">
+        <svg class="circular" viewBox="25 25 50 50">
+			<circle class="path" cx="50" cy="50" r="20" fill="none" stroke-width="2" stroke-miterlimit="10" /> </svg>
+    </div>
+    <!-- Main wrapper  -->
+    <div class="error-page" id="wrapper">
+        <div class="error-box">
+            <div class="error-body text-center">
+                <h1>404</h1>
+                {{ $exception->getMessage() }}
+                <h3 class="text-uppercase">The page you are looking for Doesn't exist</h3>
+                {{-- <p class="text-muted m-t-30 m-b-30">Bo back to the home page.</p> --}}
+                <a class="btn btn-info btn-rounded waves-effect waves-light m-b-40" href="{{ url()->previous() }}">Back to home</a> </div>
+            <footer class="footer text-center">&copy; 2018 Zebramin.</footer>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
