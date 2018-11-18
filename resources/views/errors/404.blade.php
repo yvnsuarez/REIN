40311043d64a856910049cdefc8e43fdac69b2fb
yvnsuarez <yvone.suarez@benilde.edu.ph>
2018-11-18 22:39:56 +0800

    final 404 design

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -26,162 +26,59 @@
 <![endif]-->
 </head>
 <style>
-    body{
+*{
+    transition: all 0.6s;
+}
+
+html {
+    height: 100%;
+}
+
+body{
+    background-color: #DABC20;
+    font-family:'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serifs;
+    color: black;
     margin: 0;
-    padding: 20px;
-    background-color:#FFC900;
-    font-family: arial;
-  height: 700px
 }
 
-.info{
-  position: absolute;
-  top:0;
-  left:0;
-  padding: 10px 0;
-  font-size: 110%;
-  text-transform: capitalize;
-  color:#FFC900;
-  font-weight: 700;
-  background-color:#fff;
-  width:100%;
-  text-align:center
-}
-
-.info a{
-  text-decoration: none;
-  color:#333
-}
-.info a:after{
-  content:" | ";
-  color:#FFC900
-}
-
-.info a:last-of-type:after{content:""}
-
-.box{
+#main{
+    display: table;
+    width: 100%;
+    height: 100vh;
     text-align: center;
-    position: relative;
 }
 
-.box div{
-    width: 250px;
-    height: 80px;
-    line-height: 80px;
-    color:#ffc900;
-    background-color: #fff;
-    font-size: 280%;
-    position: absolute;
-    top:490px;
-    left:40%;
-    text-transform: capitalize;
-    animation: moving 8s linear infinite;
-  -webkit-animation: moving 8s linear infinite;
-  -moz-animation: moving 8s linear infinite;
-  -o-animation: moving 8s linear infinite;
-  
-    transform-origin: 50% -400%;
-  -webkittransform-origin: 50% -400%;
-  -moz-transform-origin: 50% -400%;
-  -o-transform-origin: 50% -400%;
+.fof{
+	  display: table-cell;
+	  vertical-align: middle;
 }
 
-.box div:before{
-    content: "";
-    width: 25px;
-    height: 25px;
-    background-color:#fff;
-    border-radius: 50%;
-    display: block;
-    position: absolute;
-    left:45%;
-    top:-350px;
+.fof h1{
+	  font-size: 50px;
+	  display: inline-block;
+	  padding-right: 12px;
+	  animation: type .5s alternate infinite;
 }
 
-.box div:after{
-    content: "";
-    width: 3px;
-    height: 335px;
-    background-color: #fff;
-    display: block;
-    position: absolute;
-    left: 50%;
-    top: -330px;
-}
 
-.box p{
-    position: absolute;
-    top:560px;
-    left:38%;
-    font-weight: 700;
-    text-transform: uppercase;
-  color:#fff;
-  width: 300px
-}
-
-.box p span{
-  display: block;
-  font-size:300%
-}
-
-@keyframes moving{
-    0%,100%{
-        transform: rotate(0)
-    }
-    25%{
-        transform: rotate(3deg);
-    }
-    50%{
-        transform: rotate(-3deg)
-    }
-}
-
-@-webkit-keyframes moving{
-    0%,100%{
-        transform: rotate(0)
-    }
-    25%{
-        transform: rotate(3deg);
-    }
-    50%{
-        transform: rotate(-3deg)
-    }
-}
-
-@-moz-keyframes moving{
-    0%,100%{
-        transform: rotate(0)
-    }
-    25%{
-        transform: rotate(3deg);
-    }
-    50%{
-        transform: rotate(-3deg)
-    }
-}
-
-@-o-keyframes moving{
-    0%,100%{
-        transform: rotate(0)
-    }
-    25%{
-        transform: rotate(3deg);
-    }
-    50%{
-        transform: rotate(-3deg)
-    }
+@keyframes type{
+	  from{box-shadow: inset -3px 0px 0px #000000;}
+	  to{box-shadow: inset -3px 0px 0px transparent;}
 }
 </style>
 <body>
     {{ $exception->getMessage() }}
-    <div class="box">
-        <div>
-            Close!
-        </div>
-        <p>
-            <span>Error 404 !</span> 
-            Sorry the page you are looking for isn't found.
-        </p>
+    <div id="main">
+            <div class="fof">
+
+                    <h1>Page not found. Go back to ...</h1>
+                    <br/>
+                    <h1>
+                        <a href="{{ url()->previous() }}">
+                            <img src="{{asset('/images/REIN01.png') }}" alt="Logo" width="150px" height="58x">
+                        </a>
+                    </h1>
+            </div>
     </div>
 </body>
 
